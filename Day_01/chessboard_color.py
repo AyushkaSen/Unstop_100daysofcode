9e9971a2589a06fd5eb4ce110a48353d7bf45cb4
AyushkaSen <ayushkasen7098@gmail.com>
2026-09-07 22:29:38 +0530

    Add solution for Chessboard Cell Color problem

---
+++ Day_01/chessboard_color.py
@@ -0,0 +1,17 @@
+﻿import sys
+
+def solve():
+    s = sys.stdin.read().strip()
+    if not s:
+        return
+    
+    col = ord(s[0]) - ord('a') + 1
+    row = int(s[1])
+    
+    if (col + row) % 2 == 0:
+        print("Black")
+    else:
+        print("White")
+
+if __name__ == "__main__":
+    solve()
